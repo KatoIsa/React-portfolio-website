7302baad025774903799bcbdb1eb4a39b4ec4347
KatoIsa <katoisa1234@gmail.com>
2023-12-23 01:49:32 +0300

    UI Update

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,18 +1,20 @@
-import me from '../assets/me.png'
+import profilePic from '../assets/PortfolioProfilePic.png'
 import './css/home.css'
 
 export default function Home() {
-    return <>
-     <div className='ImageContainer'>
-        <img width={200} src={me} alt="my image" />
-        <div className='homeinfo'>
-         <div className='rings'>
-            <div className='ring'></div>
-            <div className='ring'></div>
-            <div className='ring'></div>
+   return <>
+      <div className='homeContainer'>
+         <div className='ImageContainer'>
+            <img width={200} className="profilePic" src={profilePic} alt="my image" />
+            <div className='homeinfo'>
+               <div className='rings'>
+                  <div className='ring'></div>
+                  <div className='ring'></div>
+                  <div className='ring'></div>
+               </div>
+               <p>Am Kato Isa <br /> <span>full stack dev</span></p>
+            </div>
          </div>
-         <p>Am Kato Isa <br/> a <span>full stack dev</span></p>
-        </div>
-     </div>
-    </>
+      </div>
+   </>
 }
