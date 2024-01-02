0df85a2ebb723f5e211582533b80566c3d2d6640
KatoIsa <katoisa1234@gmail.com>
2024-01-02 22:03:31 +0300

    UI -UX update ...

--- src/Components/skills.jsx
+++ src/Components/skills.jsx
@@ -1,4 +1,18 @@
 
 export default function Skills() {
-    return <h1>Skills -- under developmet </h1>
+    const spclc = "'"
+    return <div className='AboutContainer skillsContainer'>
+        <h1>SKILLS</h1>
+        <p>
+        I{spclc}ve worked on a wide range of web and mobile development Technologies. From front-end and design to backend
+        </p>
+
+        <div className="table">
+         <div className="sec-1">
+            
+         </div>
+         <div className="sec-2"></div>
+         <div className="sec-3"></div>
+        </div>
+    </div>
 }
